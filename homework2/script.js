534914afef01c1d0c23301ca7bd1af2b87a7f5ab
Serhii Veretennikov <gridis72@gmail.com>
2022-11-07 23:23:58 +0000

    completed HW2

---
+++ homework2/script.js
@@ -0,0 +1,19 @@
+// 1
+const userName = "John";
+const userSurname = "Doe";
+var userAge = 27;
+
+// 2
+const newUser = {};
+const purchasesList = [];
+var isUserOnline = true;
+var employeeSalary = 20000;
+const poinerCoordinates = {};
+
+//3
+console.log("Hello World!");
+newUser.name = userName;
+newUser.surname = userSurname;
+newUser.age = userAge
+console.log("newUser", newUser);
+console.log("newUser.name", newUser.name, "newUser.surname", newUser.surname, "newUser.age", newUser.age)

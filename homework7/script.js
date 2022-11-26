edd0fb38ed176973ad034c9d0cd1c6fd0b5ff7ac
Serhii Veretennikov <gridis72@gmail.com>
2022-11-26 15:27:36 +0000

    HW7 done

---
+++ homework7/script.js
@@ -0,0 +1,34 @@
+// 1
+
+const taskEntering = () => {
+    const tasks = {};
+
+    while (true) {
+        let taskTime = prompt("Enter task time:");
+        if (taskTime == null) break;
+        let taskText = prompt("Enter task text:");
+        if (taskText == null) break;
+        
+        tasks[taskTime] = taskText;
+    }
+    console.log(tasks);
+}
+
+taskEntering();
+// 2
+
+const salaries = {
+    John: "4300.00",
+    Ann: "5700.40",
+    Pete: "4900.95",
+};
+
+const salariesSum = (employeesObj) => {
+    let sum = 0;
+    for (let key in employeesObj) {
+        sum += +employeesObj[key] * 100;
+    }
+    return sum / 100;
+}
+
+console.log(salariesSum(salaries));
